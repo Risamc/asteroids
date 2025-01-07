03a2157bea45391e2e07cfa222bd0e7ca7fb1c5d
risamc <risamc4@gmail.com>
2025-01-07 19:48:13 +0100

    saving progress

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+from player import Player
+import player
 import pygame
 from constants import *
+from circleshape import CircleShape
 
 pygame.init()
 
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 def game_loop():
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill(000000)
+		
+		screen.fill((0, 0, 0))
+		player.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+		player.update(dt)
+		
 
 def main():
 	print("Starting asteroids!")
